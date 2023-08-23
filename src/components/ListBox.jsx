4bead50c546d499b4d2c67abb632337392412ec6
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-23 23:39:02 +0600

    MovieList component created

--- src/components/ListBox.jsx
+++ src/components/ListBox.jsx
@@ -1,6 +1,7 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import MovieList from "./MovieList";
 
-const ListBox = ({movies}) => {
+const ListBox = ({ movies }) => {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -11,24 +12,9 @@ const ListBox = ({movies}) => {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <ul className="list">
-          {movies?.map((movie) => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>🗓</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen1 && <MovieList movies={movies}/>}
     </div>
   );
-}
+};
 
-export default ListBox
+export default ListBox;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MovieList = ({movies}) => {
+  return (
+    <>
+      <ul className="list">
+        {movies?.map((movie) => (
+          <li key={movie.imdbID}>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+              <p>
+                <span>🗓</span>
+                <span>{movie.Year}</span>
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default MovieList;
