631ba83efd26f08b91f24194d1105ea007e74f26
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-23 23:42:09 +0600

    Movie component is created

---
+++ src/components/Movie.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Movie = ({movie}) => {
+  return (
+    <div>
+      <li key={movie.imdbID}>
+        <img src={movie.Poster} alt={`${movie.Title} poster`} />
+        <h3>{movie.Title}</h3>
+        <div>
+          <p>
+            <span>🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </div>
+      </li>
+    </div>
+  );
+}
+
+export default Movie
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,12 @@
 import React from "react";
+import Movie from "./Movie";
 
 const MovieList = ({movies}) => {
   return (
     <>
       <ul className="list">
         {movies?.map((movie) => (
-          <li key={movie.imdbID}>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
-            <div>
-              <p>
-                <span>🗓</span>
-                <span>{movie.Year}</span>
-              </p>
-            </div>
-          </li>
+          <Movie movie={movie}/>
         ))}
       </ul>
     </>
