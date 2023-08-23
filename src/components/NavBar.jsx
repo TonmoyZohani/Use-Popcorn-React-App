109066744e7c52bc362d2a4356c52aa1fc4f1ac7
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-23 22:57:20 +0600

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const NavBar = ({ movies }) => {
+  const [query, setQuery] = useState("");
+  return (
+    <div>
+      <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className="num-results">
+          Found <strong>{movies.length}</strong> results
+        </p>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
